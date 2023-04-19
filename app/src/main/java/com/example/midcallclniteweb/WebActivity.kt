81255cc4a3020ba027021ae82f278d874b4182e3
terrylinezyll <terrylinezyll@gmail.com>
2023-04-19 14:27:05 +0300

    my first commit

--- app/src/main/java/com/example/midcallclniteweb/WebActivity.kt
+++ app/src/main/java/com/example/midcallclniteweb/WebActivity.kt
@@ -1,11 +1,36 @@
 package com.example.midcallclniteweb
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 
 class WebActivity : AppCompatActivity() {
+    lateinit var mywebview:WebView
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        mywebview=findViewById(R.id.webView)
+        webview()
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webview(){
+        mywebview.webViewClient= WebViewClient()
+        mywebview.apply {
+            loadUrl("https://www.instagram.com/")
+            settings.javaScriptEnabled=true
+            settings.safeBrowsingEnabled=true
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (mywebview.canGoBack())mywebview.goBack()else
+        super.onBackPressed()
     }
 }
